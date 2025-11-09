ec5b46a13d9799c60e5f42c790acc69900c759fa
Erik Malo Schuessler <90277878+Brainspeck@users.noreply.github.com>
2025-11-09 03:00:24 +0000

    added role and location

--- src/js/app.js
+++ src/js/app.js
@@ -33,11 +33,13 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>${
-            variables.name === null ? "Write your name" : variables.nam
-          }</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+          <h1>${variables.name === null ? "Erik" : variables.name} ${
+    variables.lastName === null ? "Schuessler" : variables.lastName
+  }</h1>
+          <h2>${variables.role === null ? "Engineer" : variables.role}</h2>
+          <h3>${variables.city === null ? "CDMX" : variables.city} ${
+    variables.country === null ? "Mexico" : variables.country
+  }</h3>
           <ul class="position-right">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
